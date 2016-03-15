54bbca5a0e6725621e2bfdaa6817b635571daad6
Daniel Yarlett <daniel.yarlett@gmail.com>
2016-03-14 19:39:31 -0700

    Working on StringFeatures iterator.

--- src/string_codes.rs
+++ src/string_codes.rs
@@ -1,28 +1,26 @@
-struct StringFeatures<'a> {
-    string: &'a str,
+pub struct StringFeatures {
+    chars: Vec<char>,
     position: usize,
 }
 
-impl Iterator for StringFeatures {
-    type Item = &str;
-    fn next (&mut self) -> Option<&str> {
-        if self.position < self.string.chars.len() {
-            let out = &self.string.chars.nth(self.position);
-            self.position += 1;
-            Some(out)
-        } else {
-            None()
-        }
+
+impl StringFeatures {
+    pub fn new(string: &str) -> StringFeatures {
+        let chars: Vec<char> = string.chars().collect();
+        StringFeatures{ chars: chars, position: 0 }
     }
 }
 
 
-fn foo(s: &str) -> StringFeatures {
-    StringFeatures{ s: s, pos: 0 }
-}
-
-
-pub fn bar(s: &str) -> Vec<f64> {
-    let mut v: Vec<f64> = Vec::new();
-    v
-}
+// impl<'a> Iterator for StringFeatures {
+//     type Item = &'a[char];
+//     fn next (&mut self) -> Option<&[char]> {
+//         if self.position < self.chars.len() {
+//             let out = &self.chars[self.position];
+//             self.position += 1;
+//             Some(out)
+//         } else {
+//             None()
+//         }
+//     }
+// }
